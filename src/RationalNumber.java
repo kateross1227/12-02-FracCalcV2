8534c631d8b9458e9d15bfb9735cc7bb43c1b879
Kate Ross <kateross1227@gmail.com>
2014-12-03 07:59:19 -0700

    Update RationalNumber.java

--- src/RationalNumber.java
+++ src/RationalNumber.java
@@ -1,35 +1,82 @@
 // Write a class called RationalNumber that represents a fraction with an integer numerator and denominator
-// Complete the main class FracCalcV2 to create two instances of RationalNumber and use the methods
 
-public class FracCalcV2 {
-  public static void main(String[] args){
-    RationalNumber frac1 = new RationalNumber (3, 6);
-    RationalNumber frac2 = new RationalNumber (2, 8);
-    
-    System.out.println("frac1: " + frac1.toString());
-    System.out.println("frac2: " + frac2.toString());
-    
-    frac1.add(frac2);
-    System.out.println("frac1 + frac2 = " + frac1.toString());
-    
-    frac1.subtract(frac2);
-    System.out.println("frac1 - frac2 = " + frac1.toString());
-    
-    frac1.multiply(frac2);
-    System.out.println("frac1 * frac2 = " + frac1.toString());
-    
-    frac1.divide(frac2);
-    System.out.println("frac1 / frac2 = " + frac1.toString());
+import java.util.*;
+
+public class RationalNumber {
+  int num;
+  int denom;
+  
+  public RationalNumber() {
+    num = 0;
+    denom = 1;
+  }
+  
+  public RationalNumber(int numerator, int denominator) {
+    denom = denominator;
+    num = numerator;
+    if (denominator == 0) {
+      denominator = 1;
+    }    
+    if (denominator < 0) {
+      denominator *= -1;
+      numerator *= -1;
+    }
+  }
+  
+  public int getDenominator() {
+    return denom;
+  }
+  
+  public int getNumerator() {
+    return num;
+  }
+  
+  public int GCD(int first, int second) {
+    int temp;
+    while (second != 0) {
+      temp = second;
+      second = first % second;
+      first = temp;
+    }        
+    return first;
+  }
+  
+  public String toString() {
+    String s;
+    if (denom == 1) {
+      s = " " + num;
+    }
+    else {
+      s = (num / GCD(num, denom)) + "/" + (denom / GCD(num, denom));
+    }
+    return s;
+  }
+  
+  public void add(RationalNumber frac) {
+    int denom2 = frac.getDenominator();
+    int num2 = frac.getNumerator();
+    num = (num * denom2) + (num2 * denom);
+    denom *= denom2;
+  }
+  
+  public void subtract(RationalNumber frac) {
+    int denom2 = frac.getDenominator();
+    int num2 = frac.getNumerator();
+    num = (num * denom2) - (num2 * denom);
+    denom *= denom2;
+  }
+  
+  public void multiply(RationalNumber frac) {
+    int denom2 = frac.getDenominator();
+    int num2 = frac.getNumerator();
+    num *= num2;
+    denom *= denom2;
+  }
+  
+  public void divide(RationalNumber frac) {
+    int denom2 = frac.getDenominator();
+    int num2 = frac.getNumerator();
+    num *= denom2;
+    denom *= num2;
   }
 }
-// in your main method...
-//   create two instances of RationalNumber frac1 and frac2 with values 3/6 and 2/8
-//   print each RationalNumber using the toString() method
-//   - add frac2 to frac1, i.e. frac1.add(frac2);
-//     -- print frac1 using toString()
-//   - subtract frac2 from frac1
-//     -- print frac1
-//   - multiply frac1 by frac2
-//     -- print frac1
-//   - divide frac1 by frac2
-//     -- print frac1
